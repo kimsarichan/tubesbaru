e034eb79be568b48e9e8d1b5e2e418f18506d891
kimsarichan <sari.rahmawati11@gmail.com>
2015-04-08 16:06:27 +0700

    tubes pbo

---
+++ src/tubestahap1/message.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+class message  {
+	String m;
+	Date tgl;
+	String pengirim ;
+	public message(String m , Date tgl , String pengirim ){
+		this.m=m;
+		this.tgl=tgl;
+		this.pengirim=pengirim;
+	}
+	
+
+}
---
+++ src/tubestahap1/status.java
@@ -0,0 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+class status extends text{
+	public status(String stat , Date tgl){
+		super(stat,tgl);
+	}
+}
